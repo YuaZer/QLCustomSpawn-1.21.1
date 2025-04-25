9dc138497cdb269da20fd542785eed2b0a84d180
YuaZer <1109132@qq.com>
2025-04-26 07:15:43 +0800

    修复变量

--- src/main/kotlin/io/github/yuazer/qlcustomspawn/hook/ContainerHook.kt
+++ src/main/kotlin/io/github/yuazer/qlcustomspawn/hook/ContainerHook.kt
@@ -4,17 +4,19 @@ import io.github.yuazer.qlcustomspawn.api.data.ContainerApi
 import org.bukkit.entity.Player
 import taboolib.platform.compat.PlaceholderExpansion
 
-object ContainerHook:PlaceholderExpansion {
+object ContainerHook : PlaceholderExpansion {
     // 变量前缀
     override val identifier: String = "qlcustomspawn"
 
     // 变量操作
     override fun onPlaceholderRequest(player: Player?, args: String): String {
-        if (player==null){
+        if (player == null) {
             return "player null"
         }
-        return ContainerApi.getManager().get(args)?.let {
-            when (args) {
+        val containerName = args.split("<>")[0]
+        val request = args.split("<>")[1]
+        return ContainerApi.getManager().get(containerName)?.let {
+            when (request) {
                 "name" -> it.name
                 "period" -> it.period.toString()
                 "timeCount" -> it.timeCount.toString()
